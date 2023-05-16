8e3a280a0b47e8b99918927e37f560065b07d106
Dieu Hang <dieuhangdoandn@gmail.com>
2023-05-16 13:10:06 +0700

    feat: Handle controller of estate and user

---
+++ src/controllers/index.js
@@ -0,0 +1,46 @@
+import { getAllRoles } from './role.controller.js';
+import { getAllOrderStatus } from './orderStatus.controller.js';
+import { getAllEstateStatus } from './estateStatus.controller.js';
+import { getAllEstateType } from './estateType.controller.js';
+import {
+    createEstate,
+    getAllEstate,
+    getInfoEstate,
+    deleteEstate,
+    updateEstate,
+    getEstateByOwner,
+    findNearEstate
+} from './estate.controller.js';
+
+const roleController = {
+    getAllRoles
+};
+
+const orderStatusController = {
+    getAllOrderStatus
+};
+
+const estateStatusController = {
+    getAllEstateStatus
+};
+
+const estateTypeController = {
+    getAllEstateType
+};
+const estateController = {
+    createEstate,
+    getAllEstate,
+    getInfoEstate,
+    deleteEstate,
+    updateEstate,
+    getEstateByOwner,
+    findNearEstate
+};
+
+export {
+    roleController,
+    orderStatusController,
+    estateStatusController,
+    estateTypeController,
+    estateController
+};
