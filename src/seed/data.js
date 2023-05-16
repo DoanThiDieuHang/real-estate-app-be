5fd3d24cedbc88bc349276ed6a6dbe650824697a
Bùi Duy Phương <65779951+bduyphuong12@users.noreply.github.com>
2023-05-16 12:55:28 +0700

    feat: seed for BE and husky code

---
+++ src/seed/data.js
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+
+export const estateStatus = [
+    {
+        id: mongoose.Types.ObjectId(),
+        name: 'Available'
+    },
+    {
+        id: mongoose.Types.ObjectId(),
+        name: 'Sold'
+    },
+    {
+        id: mongoose.Types.ObjectId(),
+        name: 'Dealing'
+    }
+];
+
+export const estateTypes = [
+    {
+        id: mongoose.Types.ObjectId(),
+        name: 'House'
+    },
+    {
+        id: mongoose.Types.ObjectId(),
+        name: 'Apartment'
+    },
+    {
+        id: mongoose.Types.ObjectId(),
+        name: 'Building'
+    }
+];
+
+export const orderStatus = [
+    {
+        id: mongoose.Types.ObjectId(),
+        name: 'Pending',
+        estates: []
+    },
+    {
+        id: mongoose.Types.ObjectId(),
+        name: 'Confirmed',
+        estates: []
+    },
+    {
+        id: mongoose.Types.ObjectId(),
+        name: 'Done',
+        estates: []
+    }
+];
+
+export const roles = [
+    {
+        id: mongoose.Types.ObjectId(),
+        name: 'ADMIN'
+    },
+    {
+        id: mongoose.Types.ObjectId(),
+        name: 'SALER & BUYER'
+    }
+];
